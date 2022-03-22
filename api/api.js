b5205b8b6aca38756bc2dfe691963f3dd0e6d746
Myriam-Mnnr <98812962+Myriam-Mnnr@users.noreply.github.com>
2022-03-22 16:59:13 +0100

    fix connection api

--- api/api.js
+++ api/api.js
@@ -6,15 +6,8 @@ let url = "https://api.artic.edu/api/v1/artworks?page=1"
 
 
 fetch(url)
-    .then(response => {
-        if(response.ok) {
-            console.log("coucou", response.json())
-            //console.log(response.data[2].artist_title); 
-        }
-        else {
-            console.log("Erreur")
-        }
-    })
-    .catch(error => alert("Erreur : " + error)); 
+    .then(response => response.json())
+    .then(data => console.log(data.data[0].artist_title))
+    //.catch(error => alert("Erreur : " + error)); 
 
     //console.log(response)
