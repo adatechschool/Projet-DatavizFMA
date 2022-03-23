b8d93a584fe6e6072714024f107c4b8285b71b69
Félix <benoitoneilfelix@gmail.com>
2022-03-23 17:08:24 +0100

    feat add infos

--- api/api.js
+++ api/api.js
@@ -1,20 +1,26 @@
 //Art Institute of Chicago API 
-const image = document.getElementById("image")
-let pages = "1"; 
+function randomize(){
+let pages = Math.floor(Math.random() * 9669); 
 let reponse;
-let url = "https://api.artic.edu/api/v1/artworks?page=1"
-
+let url = "https://api.artic.edu/api/v1/artworks?page=" + pages
+console.log(pages)
 
 fetch(url)
-    .then(response => {
-        if(response.ok) {
-            console.log("coucou", response.json())
-            //console.log(response.data[2].artist_title); 
-        }
-        else {
-            console.log("Erreur")
-        }
-    })
-    .catch(error => alert("Erreur : " + error)); 
+.then(response => response.json())
+.then(data => inHTML(data))
+.catch(error => alert("Erreur : " + error)); 
 
-    //console.log(response)
+function inHTML(data){
+    let x = Math.floor(Math.random() * 12)
+    document.getElementById("title").innerHTML=data.data[x].title
+    document.getElementById("artist_display").innerHTML=data.data[x].artist_display
+    document.getElementById("date_display").innerHTML=data.data[x].date_display
+    document.getElementById("provenance_text").innerHTML=data.data[x].provenance_text
+    document.getElementById("dimensions").innerHTML=data.data[x].dimensions
+    document.getElementById("medium_display").innerHTML=data.data[x].medium_display
+    document.getElementById("department_title").innerHTML=data.data[x].department_title
+    document.getElementById("image").src="https://www.artic.edu/iiif/2/" + data.data[x].image_id + "/full/843,/0/default.jpg"
+}
+
+}
+randomize()
